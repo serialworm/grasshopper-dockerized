5a6794ab4083d61eb3c17d300187273302766ef0
Eric Beringer <eric.beringer@thinksolid.com>
2014-08-29 08:58:23 -0500

    Using trusty

--- bin/bootstrap_puppet.sh
+++ bin/bootstrap_puppet.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ ! -e /tmp/puppet-updated ]; then
-  wget -O /tmp/puppetlabs-release-precise.deb http://apt.puppetlabs.com/puppetlabs-release-precise.deb
-  dpkg -i /tmp/puppetlabs-release-precise.deb
+  wget -O /tmp/puppetlabs-release-trusty.deb http://apt.puppetlabs.com/puppetlabs-release-trusty.deb
+  dpkg -i /tmp/puppetlabs-release-trusty.deb
   apt-get update
   apt-get --assume-yes install puppet
 fi
